3884d66040dff117d1ee5e158cb82fbaf8b0a1e9
jukunye <jukunyes@gmail.com>
2023-02-16 15:20:39 +0000

    midified puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- *
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
