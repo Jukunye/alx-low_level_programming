002d0bc3c5c5bbbcd6072051b418d5883c3e987f
jukunye <jukunyes@gmail.com>
2023-02-17 14:04:57 +0000

    add put except q and e code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program prints alphabets a to z'
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			if ((alphabet != q) && (alphabet != e))
+			{
+			putchar(alphabet);
+			alphabet++;
+			}
+		}
+	putchar('\n');
+	return (0);
+}
