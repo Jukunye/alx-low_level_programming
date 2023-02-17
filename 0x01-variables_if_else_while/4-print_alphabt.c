3cbd086ccbbdc330bfe827e99fe03a6f0f745d05
jukunye <jukunyes@gmail.com>
2023-02-17 14:41:53 +0000

    add or

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'this program prints alphabets a to z'
+ * Description: 'this program prints alphabets a to z except q and e'
  *
  * Return: 0 Always (Success)
  */
@@ -12,11 +12,12 @@ int main(void)
 	char alphabet = 'a';
 		while (alphabet <= 'z')
 		{
-			if ((alphabet != 'q') && (alphabet != 'e'))
+			if ((alphabet == 'e') || (alphabet == 'q'))
 			{
+				continue;
+			}
 			putchar(alphabet);
 			alphabet++;
-			}
 		}
 	putchar('\n');
 	return (0);
