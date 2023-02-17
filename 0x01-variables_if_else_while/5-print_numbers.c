b3b47b339baa2db93b4f8a9a761417a09c517027
jukunye <jukunyes@gmail.com>
2023-02-17 15:18:38 +0000

    added print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program will print all numbers to base 10'
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
+
