a6d5a2e309124d2a6f2731a333106a95c6295992
jukunye <jukunyes@gmail.com>
2023-02-17 16:26:39 +0000

    add hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program prints 1 - 10 using putchar'
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
