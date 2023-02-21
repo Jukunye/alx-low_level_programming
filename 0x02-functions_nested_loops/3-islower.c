a5c54a5bc192ac9f0d4181e5fae94cbbfcd0361d
jukunye <jukunyes@gmail.com>
2023-02-21 15:22:03 +0000

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * @C: The character to be checked
+ * @c: The character to be checked
  *
  * Return: 1 for lowercase character or 0 for anything else
  */
