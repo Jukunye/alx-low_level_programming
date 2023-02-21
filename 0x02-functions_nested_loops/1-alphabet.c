cfa19de1c0d9640519c988f4188c59224aabab4b
jukunye <jukunyes@gmail.com>
2023-02-21 14:18:21 +0000

    add 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'this program will print alphabets'
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
