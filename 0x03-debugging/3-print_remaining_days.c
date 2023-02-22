2a534baeb319dc2d5e14314aeae2c94ca554068d
jukunye <jukunyes@gmail.com>
2023-02-22 10:19:07 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 	if (month >= 3 && day >= 60)
 	{
@@ -20,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 	if (month == 2 && day == 60)
 	{
