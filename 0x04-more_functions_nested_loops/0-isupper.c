45530aab54fd9e00ddde81bca5b97ab89383cdeb
jukunye <jukunyes@gmail.com>
2023-02-23 18:27:22 +0000

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,12 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: character to be checked
- * Return: 1 if c is uppercase, otherwise 0
+ * @c: character to check
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if ( c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	return (0);
 }
