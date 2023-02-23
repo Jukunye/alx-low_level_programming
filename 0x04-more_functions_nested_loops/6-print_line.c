fc86c6bf08f79014883ce3e0664a71846e7fd454
jukunye <jukunyes@gmail.com>
2023-02-23 10:22:37 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of lines to be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
