723b471e719e4d3ae073388f5da35a38fc767d77
jukunye <jukunyes@gmail.com>
2023-02-23 18:10:21 +0000

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor
+ * "612852475143"
+ * Return: Always 0
+ */
+int main(void)
+{
+	long NUM = 612852475143;
+	return (0);
+}
