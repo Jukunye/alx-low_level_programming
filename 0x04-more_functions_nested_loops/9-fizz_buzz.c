bade43d91d891f3d980b04e72476d76b0265b797
jukunye <jukunyes@gmail.com>
2023-02-23 17:56:30 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,6 +3,7 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
@@ -12,17 +13,29 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (i % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else if (x % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (x % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", x);
+		}
+
 		if (x != 100)
+		{
 			printf(" ");
+		}
 	}
-	printf("Buzz\n");
+
+	printf("\n");
 
 	return (0);
 }
