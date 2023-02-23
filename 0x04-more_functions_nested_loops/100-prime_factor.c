5679a9ba221bb3ae16614377f1205059babf7058
jukunye <jukunyes@gmail.com>
2023-02-23 18:38:43 +0000

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,6 +8,23 @@
  */
 int main(void)
 {
-	long NUM = 612852475143;
+	lont int i, n, d;
+
+	n = 612852475143;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			if (n == i)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			d = n / i;
+			n = d;
+			i = 1;
+		}
+	}
 	return (0);
 }
