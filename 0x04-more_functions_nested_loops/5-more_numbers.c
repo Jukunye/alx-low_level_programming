17f1862cc61267a6eb04d0f76e65104f152c9175
jukunye <jukunyes@gmail.com>
2023-02-23 09:38:01 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = j = 0;
+
+	for (; i < 10; i++)
+	{
+	for (; j <= 14; j++)
+	{
+		if (j >= 10)
+			_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+	}
+	}
+
+	_putchar('\n');
+}
