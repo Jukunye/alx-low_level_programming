57b11674157ba29df9e2d4e344b2671ea1c15dc8
jukunye <jukunyes@gmail.com>
2023-02-27 10:07:46 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main."
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The string to get the length of
+ * Return: The length of @s
+ */
+
+int _strlen(char *s);
+{
+	size_t length = 0;
+
+	while (*s++)
+	length++;
+	return (length);
+}
