cf0f18d17113655ecf8f35cb2a1d51039bf899b0
jukunye <jukunyes@gmail.com>
2023-03-06 14:24:36 +0000

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,16 @@
  */
 char *_strchr(char *s, char c)
 {
-    for (; *s != '\0'; s++)
-    {
-        if (*s == c)
-        {
-            return s;
-        }
-    }
-    if (*s == c)
-    {
-        return s;
-    }
-    return NULL;
+	while (*s)
+	{
+	if (*s == c)
+	{
+		return (s);
+	}
+	s++;
+	}
+	if (!c)
+		return (s);
+	return (NULL);
 }
 
