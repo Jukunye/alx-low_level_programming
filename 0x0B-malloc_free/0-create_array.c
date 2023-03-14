736d0a44a2c79e88418fbd62f278c9234049bdd7
jukunye <jukunyes@gmail.com>
2023-03-14 08:16:53 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,25 +6,25 @@
  *                it with a specific char
  * @size: is the number of elements in the array
  * @c: character to be stored in the array
- * 
+ *
  * Return: a pointer to the array, or Null if it fails
 */
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *p;
+	unsigned int i;
+	char *p;
 
-    if (size == 0)
-        return(NULL);
-    
-    p = malloc(size * sizeof(char));
-    
-    if (p == NULL)
-        return(NULL);
+	if (size == 0)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        p[i] = c;
-    
-    return(p);
+	p = malloc(size * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
 }
