515c7c953fdd06ed2b3742f90e0f73e7bab60f31
Jukunye <jukunyes@gmail.com>
2023-03-20 12:54:42 +0300

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the name of the file it was
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
