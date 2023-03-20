1aa825d266cc5cbcd58921b26f277bc98560d974
Jukunye <jukunyes@gmail.com>
2023-03-20 13:02:48 +0300

    betty_check

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,4 +3,4 @@
 
 #define PI 3.14159265359
 
-#endif
+#endif /* _PI_H */
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,6 +4,7 @@
  * main - Entry point
  *
  * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
  *
  * Return: Always 0 (Success)
 */
