8b82acc0333d101c41f00d7a2bc2209936ef9c76
Jukunye <jukunyes@gmail.com>
2023-06-09 19:28:57 +0300

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name, followed by a new line
+ * @argc: command line arguments count
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
