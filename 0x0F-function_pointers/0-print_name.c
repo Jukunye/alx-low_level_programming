3250af8bf4a956933ac86b6b3d74a86e5217f3d9
Jukunye <jukunyes@gmail.com>
2023-06-21 06:55:45 -0700

    modified

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,7 +9,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
 	f(name);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	func = get_op_func(argv[2]);
 
-	if (!func)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
