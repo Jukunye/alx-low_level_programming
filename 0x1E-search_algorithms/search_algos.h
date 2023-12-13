d1cda3235b9e672c3245abccdf06518d826b3a5c
Jukunye <jukunyes@gmail.com>
2023-12-13 19:41:33 +0300

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
